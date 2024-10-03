a9d8438e8ab2495994e1a6fbc1b7e16b815e49de
bankmotion <harris5555555555555@gmail.com>
2024-10-03 17:57:42 -0500

    updated: npm start

---
+++ dist/config.js
@@ -0,0 +1,60 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.holidays = void 0;
+exports.holidays = [
+    {
+        name: "New Year’s Day",
+        date: "2024-01-01",
+        day: "Monday",
+    },
+    {
+        name: "Birthday of Martin Luther King, Jr.",
+        date: "2024-01-15",
+        day: "Monday",
+    },
+    {
+        name: "Washington’s Birthday",
+        date: "2024-02-19",
+        day: "Monday",
+    },
+    {
+        name: "Memorial Day",
+        date: "2024-05-27",
+        day: "Monday",
+    },
+    {
+        name: "Juneteenth National Independence Day",
+        date: "2024-06-19",
+        day: "Wednesday",
+    },
+    {
+        name: "Independence Day",
+        date: "2024-07-04",
+        day: "Thursday",
+    },
+    {
+        name: "Labor Day",
+        date: "2024-09-02",
+        day: "Monday",
+    },
+    {
+        name: "Columbus Day",
+        date: "2024-10-14",
+        day: "Monday",
+    },
+    {
+        name: "Veterans Day",
+        date: "2024-11-11",
+        day: "Monday",
+    },
+    {
+        name: "Thanksgiving Day",
+        date: "2024-11-28",
+        day: "Thursday",
+    },
+    {
+        name: "Christmas Day",
+        date: "2024-12-25",
+        day: "Wednesday",
+    },
+];
